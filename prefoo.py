512fd0b4e0395f748f7768d5a9d1fbf5dc82d2e8
Piyush Bhalala <bhalalapj@gmail.com>
2019-02-04 14:48:05 +0530

    update code for binarizer

--- prefoo.py
+++ prefoo.py
@@ -9,14 +9,16 @@ import numpy as np
 from sklearn import preprocessing
 
 input_data = np.array(
-    [[3, -1.5, 3, -6.4], [0, 3, -1.3, 4.1], [1, 2.3, -2.9, 4.3]])
+    [[3, -1.5, 3, -6.4], [0, 3, -1.3, 4.1,], [1, 2.3, -2.9, 4.3], [3, -1.5, 3, -6.4], [0, 3, -1.3, 4.1], [1, 2.3, -2.9, 4.3], [3, -1.5, 3, -6.4], [0, 3, -1.3, 4.1], [1, 2.3, -2.9, 4.3]])
 
 data_standardized = preprocessing.scale(input_data)
 data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 data_scaled = data_scaler.fit_transform(input_data)
 data_normalized = preprocessing.normalize(input_data, norm='l1')
+data_binairzed = preprocessing.Binarizer(threshold=1.4).transform(input_data)
 
 print("\nMean =", data_standardized.mean(axis=0))
 print("Std Deviation =", data_standardized.std(axis=0))
 print("\n Min max scaled data =", data_scaled)
 print("L1 normalized data =", data_normalized)
+print("\nBinarized data=", data_binairzed)
